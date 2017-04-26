1ba940ca86a2e4b58a948759ab9e45cc56a80c8c
Tim Riggs <timr@inmotionhosting.com>
2017-04-26 10:44:38 -0700

    testing controller/view

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -11,4 +11,44 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+
+    public function testViewAction() {
+        $view = new ViewModel();
+
+        $view->message = "This is a test message";
+        return $view;
+    }
+
+    public function findAction() {
+        $students = \PipelinePropel\StudentQuery::create()
+            ->filterByLastName('Joseph')
+            ->find();
+        var_dump($students);exit;
+    }
+
+    public function findOneAction() {
+        $students = \PipelinePropel\StudentQuery::create()
+            ->filterByLastName('Joseph')
+            ->findOne();
+        var_dump($students);exit;
+    }
+
+    public function insertAction() {
+        $names = ['James', 'Colbert','Robert','William','Brady','Smith','Grant','Jefferson','Washington','Monroe','Allen','Ryan','Joseph'];
+
+        $firstName  = $names[array_rand($names)];
+        $lastName   = $names[array_rand($names)];
+        $studentNum = $firstName[0] . $lastName[0] . mt_rand(100000,999999); 
+
+        $birthDate = mt_rand(1950,2000) . '-' . mt_rand(1,12) . '-' . mt_rand(0,28);
+        $student = new \PipelinePropel\Student();
+        $student->setFirstName($firstName)
+            ->setLastName($lastName)
+            ->setGender('male')
+            ->setStudentNum($studentNum)
+            ->setBirthDate($birthDate)
+            ->save();
+
+        var_dump($student);exit;
+    }
 }
