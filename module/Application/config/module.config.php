503eaec39393a7318ce4b1673c7ca65a34b3ecc0
Daniel K <danielk@inmotionhosting.com>
2018-05-13 09:05:31 -0500

    Fixed error with dorm numbering.

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -2,12 +2,12 @@
 
 $dorm_routes = array();
 $dorms = array(
-    'A',
-    'B',
-    'C',
-    'D',
-    'E',
-    'F',
+    1 => 'A',
+    2 => 'B',
+    3 => 'C',
+    4 => 'D',
+    5 => 'E',
+    6 => 'F',
 );
 
 foreach ($dorms as $id => $name) {
